a85886cdd5530f67676a7dfc8cb65db185b437f2
Korotaev Nikita <nalkorotaev@sberbank.ru>
2022-07-11 17:02:32 +0300

    test

--- core/views.py
+++ core/views.py
@@ -3,30 +3,13 @@ import json
 from django.views import View
 
 from core.buttons import start_buttons
-from main import bot, dp, types
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import requests
-from django.http import HttpResponse
-from aiogram.dispatcher.webhook import SendMessage
 
+from core.static_texts import welcome, success
 from nova_bot.settings import TOKEN
-
-
-async def index(request):
-    responce = json.loads(request.body)
-    username = responce['message']['chat']['username']
-    name = responce['message']['chat']['first_name']
-    chat_id = responce['message']['chat']['id']
-    print(responce)
-    print(username)
-    await bot.send_message(
-        chat_id=chat_id,
-        text=f'Привет, {name}, а дай номер',
-        reply_markup=start_buttons,
-    )
-    return HttpResponse("POST request processed")
 
 
 class UpdateBot(View):
@@ -36,18 +19,19 @@ class UpdateBot(View):
         return super(UpdateBot, self).dispatch(*args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        print(json.loads(request.body))
         responce = json.loads(request.body)
-        username = responce['message']['chat']['username']
-        name = responce['message']['chat']['first_name']
-        chat_id = responce['message']['chat']['id']
-        print(responce)
-        # return SendMessage(
-        #     chat_id=chat_id,
-        #     text=f'Привет, {name}, а дай номер',
-        #     reply_markup=start_buttons,
-        # )
-        self.send_message(chat_id, f'Привет, {name}, а дай номер')
+        message = responce['message']
+        chat_id = message['chat']['id']
+        if message['text'] == '/start':
+            name = message['chat']['first_name']
+            self.send_message(chat_id, welcome.foramt(name))
+        elif 'contact' in message:
+            username = message['chat']['username']
+            phone = message['contact']['phone_number']
+            headers = {'Content-Type': 'application/json'}
+            data = {'phone': phone,'login': username}
+            requests.post('https://s1-nova.ru/app/private_test_python/', headers=headers, json=data)
+            self.send_message(chat_id, success)
         return JsonResponse({"ok": "POST request processed"})
 
     def get(self, request, *args, **kwargs):  # for debug
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import Dispatcher, filters
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
-from core.buttons import start_buttons, GET_CONTACT
+from core.buttons import GET_CONTACT
 from nova_bot.settings import dotenv_path, load_dotenv
 
 if os.path.exists(dotenv_path):
@@ -17,13 +17,10 @@ dp = Dispatcher(bot)
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-# webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-# WEBHOOK_HOST = f' http://127.0.0.1'
 WEBHOOK_PATH = f'/webhook/{TOKEN}/'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-# webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
@@ -36,18 +33,9 @@ async def on_shutdown(dispatcher):
     await bot.delete_webhook()
 
 
-@dp.message_handler(state='*', commands=['start'])
-async def send_welcome(msg: types.Message) -> None:
-    logging.warning(f'Recieved a message from {msg.from_user}')
-    await msg.answer(
-        f'Привет, {msg.from_user.first_name}, а дай номер',
-        reply_markup=start_buttons,
-    )
-
-
 @dp.message_handler(filters.Text(equals=GET_CONTACT), state='*')
 async def send_contact_info(msg: types.Message) -> None:
-    await msg.answer('Спасибо, что поделился номером ✨ Все прошло успешно!')
+    await msg.answer()
 
 
 if __name__ == '__main__':
