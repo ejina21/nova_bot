44c145b172eacc7a2fa1c10364f455c68a81f1a3
Korotaev Nikita <nalkorotaev@sberbank.ru>
2022-07-11 14:06:22 +0300

    test

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.views import View
-from main import bot
+from main import bot, dp, types
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -17,6 +17,10 @@ class UpdateBot(View):
 
     def post(self, request, *args, **kwargs):
         print(json.loads(request.body))
+        responce = json.loads(request.body)
+        username = responce['chat']['username']
+        print(username)
+        dp.process_update(types.Update(**responce))
         # if DEBUG:
         #     bot.process_telegram_event(json.loads(request.body))
         # else:
