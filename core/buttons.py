3c6b6d11144654bd76f4a7628404517194311dbd
Korotaev Nikita <nalkorotaev@sberbank.ru>
2022-07-11 16:46:40 +0300

    test

--- core/buttons.py
+++ core/buttons.py
@@ -1,9 +1,12 @@
+import json
+
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 GET_CONTACT = 'Поделиться номером 📞'
 
 
 button_get_contact = KeyboardButton(GET_CONTACT, request_contact=True)
-
-start_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=1)
-start_buttons.add(button_get_contact)
+reply_markup = {'keyboard': [[{'text': GET_CONTACT, 'request_contact': True}],], 'resize_keyboard': True, 'one_time_keyboard': True}
+start_buttons = json.dumps(reply_markup)
+# start_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=1)
+# start_buttons.add(button_get_contact)
